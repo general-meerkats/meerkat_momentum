c99c9f792fc3018230defe113643d3d923c221bf
Jay Schwane <jrschwane@uwalumni.com>
2017-01-20 01:30:59 -0600

    Add brand logo; add 2nd background image to test random selection

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@
 
 $(document).ready(function () {
     
-    var img_array = ['static_background.jpg'];
+    var img_array = [
+        'static_background.jpg',
+        '13.jpg'
+    ];
     var index = 0;
 
     function getBackground() {
-        index = (index + 1) % img_array.length;
+        index = Math.floor(Math.random() * img_array.length);
         $('.splash')
             .css('background-image', 'url(assets/' + img_array[index] + ')')
             .css('opacity', 1);
