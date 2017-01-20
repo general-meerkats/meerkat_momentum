660eb2f02d8fb62f66e61307ce0434b8b32e16d2
Jay Schwane <jrschwane@uwalumni.com>
2017-01-20 00:17:08 -0600

    Add quotes module; remove unused classes from grid.css; add background img fade; tweak main layout

--- js/app.js
+++ js/app.js
@@ -1,21 +1,18 @@
+/* jshint esversion:6 */
+/* globals $, document */
+
 $(document).ready(function () {
     
     var img_array = ['static_background.jpg'];
     var index = 0;
 
-    function fadeToNext() {
+    function getBackground() {
         index = (index + 1) % img_array.length;
-        $(document.body).css({
-            'background-image': 'url(assets/' + img_array[index] + ')'
-        });
-        waitNext();
+        $('.splash')
+            .css('background-image', 'url(assets/' + img_array[index] + ')')
+            .css('opacity', 1);
     }
-
-    function waitNext() {
-        setTimeout(function () {
-            fadeToNext();
-        }, 2000);
-    }
-    waitNext();
+    
+    getBackground();
     
 });
---
+++ js/quotes.js
@@ -0,0 +1,76 @@
+/* jshint esversion:6 */
+/* globals $, document */
+
+(function ($) {
+
+    'use strict';
+
+    var quoteFeature = (function quoteFeature() {
+
+        // cache DOM elements
+        var $quoteFeature = $('#quoteFeature');
+
+        // get random quote
+        function getQuote() {
+
+            // api parameters
+            var api = {
+                endpoint: 'https://quotesondesign.com/wp-json/posts',
+                params: {
+                    'filter[orderby]': 'rand',
+                    'filter[posts_per_page]': 1,
+
+                    // Date query param to make each request unique.
+                    // this hack disables browser caching of results.
+                    'processdate': (new Date()).getTime()
+                }
+            };
+
+            // do the work
+            $.getJSON(api.endpoint, api.params)
+                .then(renderQuote)
+                .catch(handleError);
+        }
+        
+        
+        // Clean quote response strings
+        function clean(str) {
+            var pTagRex = /(<([^>]+)>)|(&lt;([^>]+)&gt;)/ig,
+                
+                // temporary element never actually added to DOM.
+                // used to decode 'special html entities'
+                text = document.createElement("textarea");
+            
+            // set element = html quote string
+            text.innerHTML = str;
+            
+            // .value converts 'special entities' to regular text.
+            // .replace removes the <p> tags
+            return text.value.replace(pTagRex, '');
+        }
+
+        // render
+        function renderQuote(response) {
+            
+            var quote = clean(response[0].content)
+            $quoteFeature.html(quote);
+            // console.log(response);  // for diag
+        }
+
+        // handle error
+        function handleError(err) {
+            console.log(err);
+        }
+
+        // export public method
+        return {
+            getQuote: getQuote
+        };
+
+    }());
+    // ========== END QUOTES MODULE ========== //
+
+    // call quoteFeature public method 'getQuote()'
+    quoteFeature.getQuote();
+
+}(jQuery));
