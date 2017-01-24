9c0a0daf30ba69061d8ae5a5dad8c2bf372f51e9
KanteLabs <kamdiou@gmail.com>
2017-01-24 15:50:18 -0500

    Added weather description for day

--- js/weather.js
+++ js/weather.js
@@ -30,7 +30,7 @@ function success(position) {
 				icons == icon ? $('#icon_img').attr('src', "./assets/SVG/"+icon+".svg") : console.log(false)
 			});			
 			$('#weather-feature').append('<p class="temp">'+Math.round(data.currently.temperature) +' &degF</p>');
-			$('#weather-feature').append('<p class="location">'+data.timezone +'</p>');
+			$('#weather-feature').append('<p class="location">'+data.hourly.summary +'</p>');
 			
 		}
 	})
